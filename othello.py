1573568bc23d28368b8d706ab53522002a3352de
Tanner Thompson <kc9joc@gmail.com>
2016-06-04 13:30:14 -0600

    fixed is_valid by creating examine_board which generates a set of valid moves every turn, which a user's selection is then checked against.

--- othello.py
+++ othello.py
@@ -1,7 +1,7 @@
 #othello.py
 import numpy as np
 
-board = np.zeros((8,8))
+board = np.zeros((8,8)).astype(int)
 board[3,3] = 1
 board[4,4] = 1
 board[4,3] = -1
@@ -10,6 +10,8 @@ board[3,4] = -1
 # 0 for empty, 1 for white, -1 for black
 
 turn = 1
+valid_moves = set()
+# print 'I just assigned turn'
 
 def draw_row(row):
 	line = ''
@@ -49,45 +51,60 @@ def get_input():
 def add(tup1, tup2):
 	return tup1[0]+tup2[0], tup1[1]+tup2[1]
 	
+def mult(tup, i):
+	return tuple(np.array(tup)*i)
+	
 def on_board(space):
 	return space[0] in range(8) and space[1] in range(8)
 
 def is_valid(space):
 	if board[space] != 0:
-		raise ValueError("that spot is already taken")
+		return False
 	for direction in [(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1),(0,1)]:
 		if on_board(add(space, direction)) and board[add(space, direction)] == -turn:
 			i = 2
 			while True:
-				print i, direction
-				if (not on_board(add(space, i*direction))) or board[add(space, i*direction)] == 0:
+				if not on_board(add(space, mult(direction,i))):
 					return False
-				if board[add(space, i*direction)] == turn:
+				if board[add(space, mult(direction,i))] == 0:
+					return False
+				if board[add(space, mult(direction,i))] == turn:
 					return True
+				#otherwise look one square farther
 				i += 1
 	return False
 				
-	
-#PLAY THE GAME	
+def examine_board():
+	valid_moves.clear()
+	for i in range(8):
+		for j in range(8):
+			if is_valid((i,j)):
+				valid_moves.add((i,j))
+
+#PLAY THE GAME
 draw_board(board)
 while True:
-	while True:
+	examine_board()
+
+	while True: #getting a valid move
 		try:
 			space = get_input()
 			if space is None:
 				break
-			assert is_valid(space)
+			assert space in valid_moves
 			break
 		except ValueError as e:
 			print e
 		except AssertionError:
 			print "that move is not valid"
 	if space is None:
-		break
+		break #ends the game
+		
 		
 	board[space] = turn
 	draw_board(board)
 	turn *= -1
+
 
 
 
